5cb2d874ab57b80740de69c2354098e037edaa00
pgabin <gabin.pinos@epitech.eu>
2025-11-11 14:36:14 -0500

    Add Skill architecture and Game Scene

---
+++ Assets/Scripts/Players/SkillManager.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour
+{
+    [SerializeField] private int currentSkillIndex = 0;
+    [SerializeField] private ASkills[] skillsList;
+    private PlayerInputs inputActions;
+    private ASkills currentSkill;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputs();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.PlayerControls.Enable();
+
+        inputActions.PlayerControls.NextSkill.performed += ctx => NextSkill();
+        inputActions.PlayerControls.PreviousSkill.performed += ctx => PreviousSkill();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.PlayerControls.Disable();
+    }
+
+
+    private void Start()
+    {
+        EquipSkill(currentSkillIndex = 0);
+    }
+
+    private void EquipSkill(int skillIndex)
+    {
+        if (skillIndex >= 0 && skillIndex < skillsList.Length)
+        {
+            inputActions.PlayerControls.MainAction.performed -= ctx => currentSkill.MainAction();
+            inputActions.PlayerControls.SecondaryAction.performed -= ctx => currentSkill.SecondaryAction();
+            currentSkill = skillsList[skillIndex];
+            currentSkill.ChangeAppearance();
+            inputActions.PlayerControls.MainAction.performed += ctx => currentSkill.MainAction();
+            inputActions.PlayerControls.SecondaryAction.performed += ctx => currentSkill.SecondaryAction();
+        }
+    }
+
+    private void NextSkill()
+    {
+        currentSkillIndex = (currentSkillIndex + 1) % skillsList.Length;
+        EquipSkill(currentSkillIndex);
+    }
+
+    private void PreviousSkill()
+    {
+        currentSkillIndex = (currentSkillIndex - 1 + skillsList.Length) % skillsList.Length;
+        EquipSkill(currentSkillIndex);
+    }
+
+}
---
+++ Assets/Scripts/Skills/Skill1.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Skill1 : ASkills
+{
+    public override void MainAction()
+    {
+        Debug.Log("Skill1 Main Action Activated");
+    }
+
+    public override void SecondaryAction()
+    {
+        Debug.Log("Skill1 Secondary Action Activated");
+    }
+}
