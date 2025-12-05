6902f5843f68bc9546aeafa97e95c166c6eeee41
Alumno <alonso.tarazona@tecsup.edu.pe>
2025-12-05 13:12:52 -0500

    avancelaser

---
+++ Assets/Scripts/Skills/Shooter/Enemy.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int Health = 5;
+
+    public void Damage(int damage)
+    {
+        Health -= damage;
+
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
